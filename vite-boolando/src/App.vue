dff6118702fcdd3be157a7727fd1db192a1d8640
Limson91 <matteo.a.colautti@gmail.com>
2023-11-06 16:29:09 +0100

    add header components

--- vite-boolando/src/App.vue
+++ vite-boolando/src/App.vue
@@ -1,9 +1,22 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+import PageHeader from './components/PageHeader.vue';
+
+export default {
+  components: {
+    PaheHeader
+  },
+
+  data() {
+    return {
+      message: 'HelloWorld!'
+    }
+  }
+}
 </script>
 
 <template>
-  <div>
+  <PageHeader />
+  <!-- <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -11,19 +24,21 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
-<style scoped>
+<style>
 .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
 }
+
 .logo:hover {
   filter: drop-shadow(0 0 2em #646cffaa);
 }
+
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
 }
---
+++ vite-boolando/src/components/PageHeader.vue
@@ -0,0 +1,28 @@
+<script>
+export default {
+    data() {
+        return {
+            title: 'Boolando',
+        }
+    }
+}
+</script>
+
+<template>
+    <header class="page-header">
+        <nav class="container">
+            <div class="row">
+                <div class="col">
+                    Donna Uomo Bambino
+                </div>
+                <div class="col">
+                    <img src="./img/boolean-logo.png" alt="" height="30">
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+
+<style>
+/* CSS va qui! */
+</style>

90c3df53a3be334909a31c35729aebdd365e4bd6
Limson91 <matteo.a.colautti@gmail.com>
2023-11-08 15:42:14 +0100

    add general sass file and list header

--- vite-boolando/src/App.vue
+++ vite-boolando/src/App.vue
@@ -3,13 +3,7 @@ import PageHeader from './components/PageHeader.vue';
 
 export default {
   components: {
-    PaheHeader
-  },
-
-  data() {
-    return {
-      message: 'HelloWorld!'
-    }
+    PageHeader
   }
 }
 </script>
@@ -27,19 +21,6 @@ export default {
   <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
-<style>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style lang="scss">
+@use '../src/style/general.scss'
 </style>
